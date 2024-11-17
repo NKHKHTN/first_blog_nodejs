2315e327118d75dd8eb91dcf9c53bf2d1a194ddb
HOÀNG NGUYỄN KHÁNH <22127127@student.hcmus.edu.vn>
2024-11-17 21:35:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//route
+app.get('/', (req, res) =>  {
+    res.send('Hey there!');
+    var a = 1;
+    var b = 2;
+    var c = a + b;
+});
+app.listen(port, () => { console.log(`Example app listening at localhost:${port}`) });
